447bc562eca257bac3453ad2f12ad1051f37faa2
Van Hoang <21110828@student.hcmute.edu.vn>
2024-01-09 20:38:00 +0700

    init project

---
+++ code-be/src/test/java/com/university/codesolution/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.codesolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
