eb015a631af38e0985ceb5d1d4c296b8a7843875
AnhAnNe <info.vananne@gmail.com>
2024-02-26 11:11:19 +0700

    #13 websocket configuration

---
+++ code-be/src/main/java/com/university/codesolution/websocket/WebSocketEventListener.java
@@ -0,0 +1,27 @@
+package com.university.codesolution.websocket;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class WebSocketEventListener {
+
+    private static final Logger log = LogManager.getLogger(WebSocketEventListener.class);
+
+    @EventListener
+    public void handleWebsocketConnectListener(SessionConnectedEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        log.info("Received a new web socket connection, sessionId: `{}`", headerAccessor.getSessionId());
+    }
+
+    @EventListener
+    public void handleWebsocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        log.info("Disconnected a new web socket connection, sessionId: `{}`", headerAccessor.getSessionId());
+    }
+}
