cb63bbc864a6e83c6923c2c4963749525929c440
Van Hoang <21110828@student.hcmute.edu.vn>
2024-03-03 23:28:36 +0700

    Introduce ProblemNotFoundException

---
+++ code-be/src/main/java/com/university/codesolution/submitcode/exception/ProblemNotFoundException.java
@@ -0,0 +1,7 @@
+package com.university.codesolution.submitcode.exception;
+
+public class ProblemNotFoundException extends RuntimeException{
+    public ProblemNotFoundException(String message){
+        super(message);
+    }
+}
