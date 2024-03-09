9dbf80acf1eb39da69238e6569dca261f0b5d1cc
Van Hoang <21110828@student.hcmute.edu.vn>
2024-03-10 05:16:02 +0700

    #17 Initialize Navbar Problem Screen

--- code-fe/src/pages/SubmitCode/SubmitCodeScreen.jsx
+++ code-fe/src/pages/SubmitCode/SubmitCodeScreen.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Navbar from '~/components/Navbars/HomeNavbar';
 import ProblemScreen from '~/components/Problem/ProblemScreen';
 import EditorScreen from '~/components/Editor/EditorScreen';
-import NavProblem from '~/components/NavbarProblem/NavProblem';
+import NavbarProblem from '~/components/NavbarProblem/NavbarProblem';
 import TestCaseScreen from '~/components/TestCaseScreen/TestCaseScreen';
 import "./SubmitCodeScreen.scss"
 
@@ -18,7 +18,7 @@ function SubmitCodeScreen() {
           </div>
 
           <div className="nav__problem__layout centered">
-            <NavProblem/>
+            <NavbarProblem/>
           </div>
 
           <div className="problem__layout">
