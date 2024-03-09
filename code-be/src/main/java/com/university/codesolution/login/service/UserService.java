74c100934fca59bccd35cc6ed78de590c01c9859
quocduy <nguyendqduy@gmail.com>
2024-03-09 14:10:45 +0700

    Update login-related files

--- code-be/src/main/java/com/university/codesolution/login/service/UserService.java
+++ code-be/src/main/java/com/university/codesolution/login/service/UserService.java
@@ -1,8 +1,17 @@
 package com.university.codesolution.login.service;
 
+import com.university.codesolution.login.customenum.ERole;
 import com.university.codesolution.login.dto.UserDTO;
+import com.university.codesolution.login.entity.User;
+
+import java.util.List;
 
 public interface UserService {
     UserDTO createUser(UserDTO userDTO);
+    String login(String phoneNumber, String password, ERole eRole) throws Exception;
+    User getUserDetailsFromToken(String token )throws Exception;
     UserDTO getUserById(Long userId);
+    User updateUser(UserDTO userDTO, Long userId);
+    List<UserDTO> getAllUsers();
+    void deleteUser(int userId);
 }
