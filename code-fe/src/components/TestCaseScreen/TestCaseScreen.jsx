e477982c9e231a17d5195e9bd4657e9969c806b1
Van Hoang <21110828@student.hcmute.edu.vn>
2024-03-09 21:59:36 +0700

    #17 Implement toolbar for testcase screen and delete redundant folders

--- code-fe/src/components/TestCaseScreen/TestCaseScreen.jsx
+++ code-fe/src/components/TestCaseScreen/TestCaseScreen.jsx
@@ -26,43 +26,44 @@ function TestCaseScreen( {result} ) {
           <span>Last Testcase</span>
         </button>
       </div>
-      <div className={`testcase__result__details 
-          ${activeTab === 'result' ? '' : 'hidden'}`}>
-        {result === "" && <h3 className="centered">You must run your code first.</h3>}
-        {result !== "" && (
-          <>
-            <h3 className="status">
-              <p className={result.status === "ACCEPTED" ? "success__color" : "failed__color"}>
-                {result.status}
-              </p>
-            </h3>
-            <div className="right-border"></div>
-            <p className="status">
-              <h3 className="centered">Message</h3>
-              <p className="centered">{result.message}</p>
-            </p>
-            <div className="right-border"></div>
-            <p className="status">
-              <h3 className="centered">Testcase</h3>
-              <p className="centered">{result.passedTestcase} / {result.maxTestcase}</p>
-            </p>
-            <div className="right-border"></div>
-            <p className="status">
-              <h3 className="centered">Runtime</h3>
-              <p className="centered">{result.runtime}s</p>
-            </p>
-            <div className="right-border"></div>
-            <p className="status">
-              <h3 className="centered">Memory Usage</h3>
-              <p className="centered">{result.memory}MB</p>
-            </p>
-          </>
-        )}
-      </div>
-
-      <div className={`last_testcase__details 
-          ${activeTab === 'result' ? 'hidden' : ''}`}>
-        This is last testcase
+      <div className="container">
+        <div className={`testcase__result__details
+                  ${activeTab === 'result' ? '' : 'hidden'}`}>
+            {result === "" && <h3 className="centered">You must run your code first.</h3>}
+            {result !== "" && (
+              <>
+                <h3 className="status">
+                  <p className={result.status === "ACCEPTED" ? "success__color" : "failed__color"}>
+                    {result.status}
+                  </p>
+                </h3>
+                <div className="right-border"></div>
+                <p className="status">
+                  <h3 className="centered">Message</h3>
+                  <p className="centered">{result.message}</p>
+                </p>
+                <div className="right-border"></div>
+                <p className="status">
+                  <h3 className="centered">Testcase</h3>
+                  <p className="centered">{result.passedTestcase} / {result.maxTestcase}</p>
+                </p>
+                <div className="right-border"></div>
+                <p className="status">
+                  <h3 className="centered">Runtime</h3>
+                  <p className="centered">{result.runtime}s</p>
+                </p>
+                <div className="right-border"></div>
+                <p className="status">
+                  <h3 className="centered">Memory Usage</h3>
+                  <p className="centered">{result.memory}MB</p>
+                </p>
+              </>
+            )}
+        </div>
+          <div className={`last_testcase__details
+              ${activeTab === 'result' ? 'hidden' : ''}`}>
+              <h3 className="centered">This is last testcase</h3>
+          </div>
       </div>
     </>
   )
