ff458759176918e9f4aa20b097bd84d7feeb72ef
Van Hoang <21110828@student.hcmute.edu.vn>
2024-03-13 20:49:31 +0700

    #17 Implement navigate between description, discuss and submissions screen + use context hook

---
+++ code-fe/src/components/DiscussesProblem/DiscussesScreen.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function DiscussesScreen() {
+    return <div>This is DiscussesScreen Page</div>;
+}
+export default DiscussesScreen;
--- code-fe/src/pages/SubmitCode/SubmitCodeScreen.jsx
+++ code-fe/src/pages/SubmitCode/SubmitCodeScreen.jsx
@@ -1,41 +1,59 @@
-import React, { useState } from 'react';
+import React, {createContext, useState} from 'react';
 import ProblemScreen from '~/components/Problem/ProblemScreen';
 import EditorScreen from '~/components/Editor/EditorScreen';
-import NavbarProblem from '~/components/NavbarProblem/ProblemNavbar/NavbarProblem';
+import NavbarProblem from '~/components/Navbars/NavbarProblem/ProblemNavbar/NavbarProblem';
 import TestCaseScreen from '~/components/TestCaseScreen/TestCaseScreen';
-import MainNavbar from "~/components/NavbarProblem/MainNavbar/MainNavbar";
+import MainNavbar from "~/components/Navbars/NavbarProblem/MainNavbar/MainNavbar";
 import "./SubmitCodeScreen.scss"
+import DiscussesScreen from "~/components/DiscussesProblem/DiscussesScreen";
+import SubmissionScreen from "~/components/Submissions/SubmissionScreen";
+
+export const AppContext = createContext(null);
 
 function SubmitCodeScreen() {
-  const [result, setResult] = useState("")
+    const [result, setResult] = useState("")
+    const [activeMenuItem, setActiveMenuItem] = useState("Description")
 
-  return (
-    <React.Fragment>
-      <section>
-        <div className="layout">
-          <div className="nav__layout centered">
-            <MainNavbar/>
-          </div>
+    const renderActiveScreen = () => {
+        switch (activeMenuItem) {
+            case "Description":
+                return <ProblemScreen/>;
+            case "Discusses":
+                return <DiscussesScreen/>;
+            case "Submissions":
+                return <SubmissionScreen/>;
+        }
+    };
 
-          <div className="nav__problem__layout centered">
-            <NavbarProblem/>
-          </div>
+    return (
+        <AppContext.Provider value={{
+            result, setResult,
+            activeMenuItem, setActiveMenuItem}}>
+            <section>
+                <div className="layout">
+                    <div className="nav__layout centered">
+                        <MainNavbar/>
+                    </div>
 
-          <div className="problem__layout">
-            <ProblemScreen/>
-          </div>
+                    <div className="nav__problem__layout centered">
+                        <NavbarProblem/>
+                    </div>
 
-          <div className="editor__layout">
-            <EditorScreen setResult={setResult}/>
-          </div>
+                    <div className="problem__layout">
+                        {renderActiveScreen()}
+                    </div>
 
-          <div className="testcases_layout">
-            <TestCaseScreen result={result}/>
-          </div>
-        </div>
-      </section>
-    </React.Fragment>
-  )
+                    <div className="editor__layout">
+                        <EditorScreen/>
+                    </div>
+
+                    <div className="testcases_layout">
+                        <TestCaseScreen/>
+                    </div>
+                </div>
+            </section>
+        </AppContext.Provider>
+    )
 }
 
 export  default SubmitCodeScreen
