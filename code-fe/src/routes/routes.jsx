79dab3aa31490458e8370ec725705fcea634c5b3
AnhAnNe <info.vananne@gmail.com>
2024-03-23 00:16:35 +0700

    add ContestInvitation UI

--- code-fe/src/routes/routes.jsx
+++ code-fe/src/routes/routes.jsx
@@ -2,10 +2,12 @@ import config from '~/config';
 
 import Home from '~/pages/Home';
 import SignIn from '~/pages/SignIn';
+import ContestInvitation from '~/pages/ContestInvitation/ContestInvitation';
 
 const publicRoutes = [
   { path: config.routes.home, component: Home },
   { path: config.routes.login, component: SignIn },
+  { path: config.routes.contest_invitation, component: ContestInvitation },
 ];
 
 const privateRoutes = [];
