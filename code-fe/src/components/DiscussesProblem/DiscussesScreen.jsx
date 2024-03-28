71533fe4d6e15f2a94ef335d1b9310f5f046e6ca
Van Hoang <21110828@student.hcmute.edu.vn>
2024-03-28 16:37:19 +0700

    #17 Init UI discussion in problem

--- code-fe/src/components/DiscussesProblem/DiscussesScreen.jsx
+++ code-fe/src/components/DiscussesProblem/DiscussesScreen.jsx
@@ -1,6 +1,65 @@
-import React from 'react';
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Divider from '@mui/material/Divider';
+import './DiscussesScreen.scss'
+import DisCussContent from './DiscussContent/DisscussContent'
 
-function DiscussesScreen() {
-    return <div>This is DiscussesScreen Page</div>;
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? 'transparent' : '#2c2929',
+    ...theme.typography.body2,
+    maxHeight: 300,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: '#d3c9c9',
+    size: 20
+}));
+
+const theme = createTheme({
+    components: {
+        MuiStack: {
+            defaultProps: {
+                useFlexGap: true,
+            },
+        },
+    },
+});
+
+
+export default function DiscussesScreen() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Stack
+                spacing={2}
+                className="discuss__layout"
+                divider={<Divider orientation="horizontal" flexItem />}>
+                <Item>
+                    <DisCussContent/>
+                </Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+                <Item><DisCussContent/></Item>
+            </Stack>
+        </ThemeProvider>
+    );
 }
-export default DiscussesScreen;
