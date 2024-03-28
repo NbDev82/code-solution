6c0c1252efe65b143b2c1a18a45530bf2c0d1041
Van Hoang <21110828@student.hcmute.edu.vn>
2024-03-28 16:37:19 +0700

    #17 Init UI discussion in problem

---
+++ code-fe/src/components/DiscussesProblem/DiscussContent/DisscussContent.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function DiscussesScreen() {
+    return (
+        <div>
+            <div>This is Disscuss Content</div>
+        </div>
+    );
+}
