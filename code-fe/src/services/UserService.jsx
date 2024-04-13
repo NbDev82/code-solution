5e846ca6c9f4b63b5eb9a3d91dd00dc60e74c0b1
quocduy <nguyendqduy@gmail.com>
2024-04-13 07:28:29 +0700

    Updated package.json and modified several files related to routing and sign-in functionality

---
+++ code-fe/src/services/UserService.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+export const BASE_URL = 'http://localhost:8000/api/';
+
+const customAxios = axios.create({
+  baseURL: BASE_URL,
+});
+export const signUp = (user) => {
+  return customAxios.post('/register', user).then((response) => response.data);
+};
+
+export const loginUser = (loginDetail) => {
+  debugger;
+  return customAxios.post('/login', loginDetail).then((response) => response.data);
+};
+
+export const getUser = (userId) => {
+  return customAxios.get(`/users/${userId}`).then((resp) => resp.data);
+};
