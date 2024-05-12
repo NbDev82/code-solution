a37e7aeffa3a719c6beff680dbdc7d0cb06012a0
AnhAnNe <info.vananne@gmail.com>
2024-05-12 17:02:33 +0700

    delete contest

--- code-fe/src/pages/ContestsManagement/ContestsManagement.jsx
+++ code-fe/src/pages/ContestsManagement/ContestsManagement.jsx
@@ -1,22 +1,7 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  Flex,
-  Heading,
-  Stack,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-} from '@chakra-ui/react';
+import { Box, Card, CardBody, Flex, Stack, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import MainNavbar from '~/components/Navbars/MainNavbar';
-import RankingList from '~/components/RankingList/RankingList';
 import GlobalContestList from '~/components/GlobalContestList/GlobalContestList';
 import MyContestList from '~/components/MyContestList/MyContestList';
 import Footer from '~/components/Footer';
@@ -58,24 +43,6 @@ const ContestsManagement = () => {
               </Tabs>
             </CardBody>
           </Stack>
-        </Card>
-
-        <Card overflow="hidden" variant="elevated" p="10px" borderRadius="3xl" boxShadow="xl" w="auto" h="fit-content">
-          <CardBody>
-            <Heading noOfLines={1} size="md" align="center" justify="center">
-              Top 10 global players
-            </Heading>
-
-            <Box mt={10} height="100%">
-              <RankingList />
-            </Box>
-          </CardBody>
-
-          <CardFooter align="center" justify="center">
-            <Button colorScheme="customBlue" variant="link" fontSize="xs">
-              View more
-            </Button>
-          </CardFooter>
         </Card>
       </Flex>
 
